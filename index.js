751d331fd98dc22ad691d39b3eae735ed565ce66
CoderShanto804 <st.shanto804@gmail.com>
2026-05-29 15:23:56 +0600

    Create a Minecraft bot with mineflayer
    Implemented a bot using mineflayer to connect to a Minecraft server.

---
+++ index.js
@@ -0,0 +1,23 @@
+const mineflayer = require('mineflayer');
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: 'dynamic-8.magmanode.com', // ম্যাগমানোডের নতুন আইপি
+        port: 25712,                     // ম্যাগমানোডের নতুন পোর্ট
+        username: 'DarkplayBot',         
+        version: '1.21.1'                
+    });
+
+    bot.on('spawn', () => {
+        console.log('DarkplayBot সফলভাবে সার্ভারে ঢুকে পড়েছে, ভাই!');
+    });
+
+    bot.on('end', () => {
+        console.log('বট ডিসকানেক্ট হয়েছে! ৫ সেকেন্ড পর আবার ট্রাই করছে...');
+        setTimeout(() => createBot(), 5000);
+    });
+
+    bot.on('error', (err) => console.log('Error হয়েছে: ', err));
+}
+
+createBot();
